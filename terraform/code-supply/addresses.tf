93c2ee36fa937b9372c5d03999eeb06b87765797
Andrew Bruce <me@andrewbruce.net>
2019-11-30 23:04:33 +0000

    Remove Kubernetes

--- terraform/code-supply/addresses.tf
+++ terraform/code-supply/addresses.tf
@@ -1,10 +1,3 @@
-resource "google_compute_address" "istio-ingress-production" {
-  name         = "istio-ingress-ip-production"
-  address      = "35.189.105.191"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
-}
-
 resource "google_compute_address" "multi-vhost" {
   name         = "multi-vhost"
   address_type = "EXTERNAL"
