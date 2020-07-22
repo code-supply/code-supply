8b920f0689474b22f3505e03eb7f833904f7e12a
Andrew Bruce <me@andrewbruce.net>
2020-07-22 22:26:53 +0100

    Add scopes and management options to node pool

--- terraform/cluster-london.tf
+++ terraform/cluster-london.tf
@@ -26,5 +26,19 @@ resource "google_container_node_pool" "london_c" {
 
   node_config {
     machine_type = "g1-small"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 }
