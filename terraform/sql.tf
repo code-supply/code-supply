1171fead4de3b61489202a654e50e12f28927e0c
Andrew Bruce <me@andrewbruce.net>
2020-07-23 14:32:14 +0100

    Can connect to Cloud SQL using client cert

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,3 +1,7 @@
+variable "andrew_ip" {
+  default = "81.187.237.24"
+}
+
 resource "google_sql_database_instance" "shared" {
   name             = "shared"
   database_version = "POSTGRES_12"
@@ -15,7 +19,23 @@ resource "google_sql_database_instance" "shared" {
     backup_configuration {
       enabled = true
     }
+
+    ip_configuration {
+      require_ssl = true
+
+      authorized_networks {
+        name  = "andrew"
+        value = var.andrew_ip
+      }
+    }
   }
+}
+
+resource "google_sql_user" "andrew" {
+  name     = "andrew"
+  instance = google_sql_database_instance.shared.name
+  host     = var.andrew_ip
+  password = "changeme"
 }
 
 resource "google_sql_ssl_cert" "shared_andrew" {
@@ -33,4 +53,8 @@ output "sql_shared_andrew_cert" {
 
 output "sql_shared_andrew_ca_cert" {
   value = google_sql_ssl_cert.shared_andrew.server_ca_cert
+}
+
+output "sql_shared_public_ip" {
+  value = google_sql_database_instance.shared.public_ip_address
 }
