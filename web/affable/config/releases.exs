6dcfe28dfdd4c6306ccefef30d873500d2d3c133
Andrew Bruce <me@andrewbruce.net>
2020-07-24 16:06:41 +0100

    Actually start the web server for releases

--- web/affable/config/releases.exs
+++ web/affable/config/releases.exs
@@ -28,14 +28,5 @@ config :affable, AffableWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :affable, AffableWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+  secret_key_base: secret_key_base,
+  server: true
