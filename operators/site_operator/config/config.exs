92c1053730597ff01e4982cc84bc606c21c3a074
Andrew Bruce <me@andrewbruce.net>
2020-07-31 08:01:23 +0100

    Configure site operator to remove example.com
    Must use atom for cluster_name
    Use k8s config, instead of older bonny config copied from tutorial

--- operators/site_operator/config/config.exs
+++ operators/site_operator/config/config.exs
@@ -12,13 +12,6 @@ config :bonny,
   controllers: [
     SiteOperator.Controller.V1.Site
   ],
-  cluster_name: "default",
+  cluster_name: :default,
   namespace: :all,
-  group: "site-operator.code.supply",
-
-  # Bonny will default to using your current-context, optionally set cluster: and user: here.
-  # kubeconf_opts: [cluster: "my-cluster", user: "my-user"]
-  kubeconf_opts: [
-    cluster: "gke_code-supply_europe-west1-b_pink",
-    user: "gke_code-supply_europe-west1-b_pink"
-  ]
+  group: "site-operator.code.supply"
