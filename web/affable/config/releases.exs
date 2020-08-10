ff257fe3fb1c290a4f77895a7bf42b4079d66ebd
Andrew Bruce <me@andrewbruce.net>
2020-08-11 00:12:13 +0100

    Configure sendgrid base URI to match docs
    See my complaint: https://github.com/thoughtbot/bamboo/issues/544

--- web/affable/config/releases.exs
+++ web/affable/config/releases.exs
@@ -33,6 +33,7 @@ config :affable, AffableWeb.Endpoint,
 
 config :affable, Affable.Mailer,
   adapter: Bamboo.SendGridAdapter,
+  sendgrid_base_uri: "https://api.sendgrid.com/v3/",
   api_key:
     System.get_env("SENDGRID_API_KEY") ||
       raise("""
