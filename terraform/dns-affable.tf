29bc964ced2e2f1b3dad1d6dcde7a7a01389fe85
Andrew Bruce <me@andrewbruce.net>
2020-08-13 16:12:42 +0100

    Wildcard DNS for platform, including www

--- terraform/dns-affable.tf
+++ terraform/dns-affable.tf
@@ -11,10 +11,10 @@ resource "google_dns_record_set" "affable-root" {
   type         = "A"
 }
 
-resource "google_dns_record_set" "affable-www" {
-  name         = "www.${google_dns_managed_zone.affable.dns_name}"
+resource "google_dns_record_set" "wildcard" {
+  name         = "*.${google_dns_managed_zone.affable.dns_name}"
   managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [google_dns_record_set.affable-root.name]
-  ttl          = 300
-  type         = "CNAME"
+  rrdatas      = ["35.195.27.167"]
+  ttl          = 1800
+  type         = "A"
 }
