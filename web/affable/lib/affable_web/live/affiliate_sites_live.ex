4034f268b33025778d67a65afb9fa9a2267e1f31
Andrew Bruce <me@andrewbruce.net>
2020-08-18 23:53:39 +0100

    Up / down icons

--- web/affable/lib/affable_web/live/affiliate_sites_live.ex
+++ web/affable/lib/affable_web/live/affiliate_sites_live.ex
@@ -6,6 +6,8 @@ defmodule AffableWeb.AffiliateSitesLive do
   alias Affable.Sites
   alias Affable.Sites.Site
 
+  import AffableWeb.IconHelpers
+
   def mount(%{"id" => id}, %{"user_token" => token}, socket) do
     case Accounts.get_user_by_session_token(token) do
       nil ->
