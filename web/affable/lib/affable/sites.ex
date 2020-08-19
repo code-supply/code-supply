d555535ed6bafbde7f911a03fa8d613ae9b6164d
Andrew Bruce <me@andrewbruce.net>
2020-08-19 22:30:29 +0100

    Default items have images

--- web/affable/lib/affable/sites.ex
+++ web/affable/lib/affable/sites.ex
@@ -53,52 +53,78 @@ defmodule Affable.Sites do
       %Item{
         position: 1,
         name: "Golden Delicious",
-        description: "Yellow. Nothing like Red Delicious."
+        description: "Yellow. Nothing like Red Delicious.",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/0/09/Mele_golden.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Mele_golden.jpg"
       },
       %Item{
         position: 2,
         name: "Gala",
-        description: "Red. Offspring of Red D and Kidd's Orange."
+        description: "Red. Offspring of Red D and Kidd's Orange.",
+        image_url:
+          "https://upload.wikimedia.org/wikipedia/commons/a/ab/2015-02-xx_Gala_%28apple%29.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:2015-02-xx_Gala_(apple).jpg"
       },
       %Item{
         position: 3,
         name: "Bramley",
-        description: "Nice in a pie."
+        description: "Nice in a pie.",
+        image_url:
+          "https://upload.wikimedia.org/wikipedia/commons/5/52/Bramley%27s_Seedling_Apples.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Bramley%27s_Seedling_Apples.jpg"
       },
       %Item{
         position: 4,
         name: "Red Prince",
-        description: "Holland made an apple. It's kinda red."
+        description: "Holland made an apple. It's kinda red.",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Red_Prince_Aepfel.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Red_Prince_Aepfel.jpg"
       },
       %Item{
         position: 5,
         name: "Greensleeves",
-        description: "Parents are Golden D and James Grieve. That naughty James."
+        description: "Parents are Golden D and James Grieve. That naughty James.",
+        image_url:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d1/Greensleeves_on_tree%2C_National_Fruit_Collection_%28acc._1980-077%29.jpg",
+        url:
+          "https://commons.wikimedia.org/wiki/File:Greensleeves_on_tree,_National_Fruit_Collection_(acc._1980-077).jpg"
       },
       %Item{
         position: 6,
         name: "Red Delicious",
-        description: "Dark Red. Popular in the states. Don't cook with it."
+        description: "Dark Red. Popular in the states. Don't cook with it.",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Red_Delicious_apples.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Red_Delicious_apples.jpg"
       },
       %Item{
         position: 7,
         name: "Pink Lady",
-        description: "From the 70's. Light red / pink. Tasty."
+        description: "From the 70's. Light red / pink. Tasty.",
+        image_url:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b8/Pink_lady_apples%2C_Thulimbah%2C_Granite_Belt%2C_Queensland%2C_2015_02.jpg",
+        url:
+          "https://commons.wikimedia.org/wiki/File:Pink_lady_apples,_Thulimbah,_Granite_Belt,_Queensland,_2015_02.jpg"
       },
       %Item{
         position: 8,
         name: "Discovery",
-        description: "Sweet flavour. English."
+        description: "Sweet flavour. English.",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Discovery_apples.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Discovery_apples.jpg"
       },
       %Item{
         position: 9,
-        name: "Crispin",
-        description: "Japanese, offshoot of Golden D and Indo."
+        name: "Braeburn",
+        description: "Common in the UK supermarkets. Pretty good!",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Braeburn2008.jpg",
+        url: "https://commons.wikimedia.org/wiki/File:Braeburn2008.jpg"
       },
       %Item{
         position: 10,
-        name: "Gavin",
-        description: "Is this one a joke or what?"
+        name: "Cox's Orange Pippin",
+        description: "Kind of a big deal in the UK.",
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/e/ed/Cox_orange_renette2.JPG",
+        url: "https://commons.wikimedia.org/wiki/File:Cox_orange_renette2.JPG"
       }
     ]
     |> Enum.map(&Item.changeset(&1, %{}))
