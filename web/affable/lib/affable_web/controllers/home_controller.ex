094b0a6ef6c87220c62117f86252a44adb88df7d
Andrew Bruce <me@andrewbruce.net>
2020-08-26 19:48:02 +0100

    Show nodes on affable home page

--- web/affable/lib/affable_web/controllers/home_controller.ex
+++ web/affable/lib/affable_web/controllers/home_controller.ex
@@ -2,6 +2,8 @@ defmodule AffableWeb.HomeController do
   use AffableWeb, :controller
 
   def show(conn, _params) do
-    render(conn, "show.html")
+    node = inspect(node())
+    nodes = inspect(Node.list())
+    render(conn, "show.html", node: node, nodes: nodes)
   end
 end
