42fe299fdc0f2371515c7dd305d49b2250fa4c3e
Andrew Bruce <me@andrewbruce.net>
2020-09-08 23:07:20 +0100

    Remove node info from affable

--- web/affable/lib/affable_web/controllers/home_controller.ex
+++ web/affable/lib/affable_web/controllers/home_controller.ex
@@ -2,8 +2,6 @@ defmodule AffableWeb.HomeController do
   use AffableWeb, :controller
 
   def show(conn, _params) do
-    node = inspect(node())
-    nodes = inspect(Node.list())
-    render(conn, "show.html", node: node, nodes: nodes)
+    render(conn, "show.html")
   end
 end
