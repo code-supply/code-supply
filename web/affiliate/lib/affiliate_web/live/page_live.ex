00c1bf49638830183dcb1612f335f73ff0de76d8
Andrew Bruce <me@andrewbruce.net>
2020-11-18 21:06:04 +0000

    Remove node inspection code from affiliate

--- web/affiliate/lib/affiliate_web/live/page_live.ex
+++ web/affiliate/lib/affiliate_web/live/page_live.ex
@@ -5,12 +5,10 @@ defmodule AffiliateWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    node = inspect(node())
-    nodes = inspect(Node.list())
     {pubsub, topic} = SiteState.subscription_info()
     %{name: _} = site = SiteState.site()
     Phoenix.PubSub.subscribe(pubsub, topic)
-    {:ok, assign(socket, node: node, nodes: nodes, site: site, page_title: site.name)}
+    {:ok, assign(socket, site: site, page_title: site.name)}
   end
 
   @impl true
