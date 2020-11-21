7bb330648a5d9580d1a4d889c6f5aa5054aa9a52
Andrew Bruce <me@andrewbruce.net>
2020-11-21 20:13:44 +0000

    Sites have /preview and /
    / only serves 'published' content

--- web/affiliate/lib/affiliate_web/live/preview_live.ex
+++ web/affiliate/lib/affiliate_web/live/preview_live.ex
@@ -1,4 +1,4 @@
-defmodule AffiliateWeb.PageLive do
+defmodule AffiliateWeb.PreviewLive do
   use AffiliateWeb, :live_view
 
   alias Affiliate.SiteState
@@ -6,13 +6,13 @@ defmodule AffiliateWeb.PageLive do
   @impl true
   def mount(_params, _session, socket) do
     {pubsub, topic} = SiteState.subscription_info()
-    %{published: %{"name" => page_title} = site} = SiteState.get()
+    %{preview: %{"name" => page_title} = site} = SiteState.get()
     Phoenix.PubSub.subscribe(pubsub, topic)
     {:ok, assign(socket, site: site, page_title: page_title)}
   end
 
   @impl true
-  def handle_info(%{published: site}, socket) do
+  def handle_info(%{preview: site}, socket) do
     {:noreply, assign(socket, site: site, page_title: site["name"])}
   end
 end
--- web/affiliate/lib/affiliate_web/router.ex
+++ web/affiliate/lib/affiliate_web/router.ex
@@ -18,6 +18,7 @@ defmodule AffiliateWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/preview", PreviewLive, :index
   end
 
   # Other scopes may use custom stacks.
