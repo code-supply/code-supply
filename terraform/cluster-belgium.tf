4842c9fed2f1d8b8d0cc83d81717b398bb866569
Andrew Bruce <me@andrewbruce.net>
2020-12-14 18:59:12 +0000

    New node pool
    2xe2-mediums instead of 4xe2-smalls

--- terraform/cluster-belgium.tf
+++ terraform/cluster-belgium.tf
@@ -50,17 +50,17 @@ resource "google_container_cluster" "belgium_pink" {
   }
 }
 
-resource "google_container_node_pool" "belgium_c" {
-  name       = "chips"
+resource "google_container_node_pool" "belgium_d" {
+  name       = "waffles"
   location   = "europe-west1-b"
   cluster    = google_container_cluster.belgium_pink.name
-  node_count = 4
+  node_count = 2
 
   node_config {
     preemptible  = true
     disk_size_gb = 10
 
-    machine_type = "e2-small"
+    machine_type = "e2-medium"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
