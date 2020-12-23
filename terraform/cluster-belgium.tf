28cf9205e19bb1b21e43fd4cbb73c2d977eb9dee
Andrew Bruce <me@andrewbruce.net>
2020-12-23 17:27:43 +0000

    Separate node pools for 2 zones
    Help prevent single-zone failure

--- terraform/cluster-belgium.tf
+++ terraform/cluster-belgium.tf
@@ -50,11 +50,44 @@ resource "google_container_cluster" "belgium_pink" {
   }
 }
 
-resource "google_container_node_pool" "belgium_d" {
-  name       = "waffles"
+resource "google_container_node_pool" "waffles" {
+  name     = "waffles"
+  location = "europe-west1-b"
+  cluster  = google_container_cluster.belgium_pink.name
+
+  node_count = 2
+
+  node_config {
+    preemptible  = true
+    disk_size_gb = 10
+
+    machine_type = "e2-small"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    tags = [var.standard_node_tag]
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+
+resource "google_container_node_pool" "peas" {
+  name       = "peas"
   location   = "europe-west1-b"
   cluster    = google_container_cluster.belgium_pink.name
-  node_count = 3
+  node_count = 2
+
+  node_locations = ["europe-west1-c"]
 
   node_config {
     preemptible  = true
