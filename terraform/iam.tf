01b8c31b2760bec624e8dd9743721b94a596b140
Andrew Bruce <me@andrewbruce.net>
2021-01-03 23:57:44 +0000

    Affable GCE service account
    Given workloadIdentityUser to connect it with a k8s service account

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,3 +11,17 @@ resource "google_project_iam_binding" "dns-cert-manager" {
     "serviceAccount:${google_service_account.dns-cert-manager.email}",
   ]
 }
+
+resource "google_service_account" "affable" {
+  account_id   = "affable"
+  display_name = "Affable application"
+}
+
+resource "google_project_iam_binding" "affable-workload-identity" {
+  project = data.google_project.project.id
+  role    = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:${google_service_account.affable.email}",
+  ]
+}
