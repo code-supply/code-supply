d10d16df738a799d15b610eb2d7bcbf0e9aa8a0d
Andrew Bruce <me@andrewbruce.net>
2021-05-21 20:44:21 +0100

    Use workload identity to access CloudSQL

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,6 +17,12 @@ resource "google_service_account" "affable" {
   display_name = "Affable application"
 }
 
+resource "google_project_iam_member" "affable-cloud-sql" {
+  project = data.google_project.project.id
+  role    = "roles/cloudsql.client"
+  member  = "serviceAccount:${google_service_account.affable.email}"
+}
+
 resource "google_service_account" "affable-dev" {
   account_id   = "affable-dev"
   display_name = "Affable application in development"
