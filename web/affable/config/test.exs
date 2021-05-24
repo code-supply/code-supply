35349182ff02985fdf4b03415b6016f57de75a68
Andrew Bruce <me@andrewbruce.net>
2021-05-24 15:55:55 +0100

    Make test db pool bigger, or something.
    This avoids errors like:
    
    connection not available and request was dropped from queue after...
    
    See https://elixirforum.com/t/dbconnection-error/21478

--- web/affable/config/test.exs
+++ web/affable/config/test.exs
@@ -18,7 +18,9 @@ config :affable, Affable.Repo,
   password: "postgres",
   database: "affable_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10,
+  queue_target: 5000
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
