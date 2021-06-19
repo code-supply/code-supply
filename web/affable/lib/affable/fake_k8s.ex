029d91f630155984c7a3c46bd76646dc019a0656
Andrew Bruce <me@andrewbruce.net>
2021-06-19 22:54:29 +0100

    Remove ready() in FakeK8s
    This was useful until the way we broadcast site updates changed. For
    now, let's hit the API site controller to set availability in dev.

--- web/affable/lib/affable/fake_k8s.ex
+++ web/affable/lib/affable/fake_k8s.ex
@@ -22,16 +22,4 @@ defmodule Affable.FakeK8s do
     msg = "FakeK8s: would have patched #{inspect(resource)}"
     {IO.puts(msg), msg}
   end
-
-  def ready() do
-    for site <- Site |> Repo.all() do
-      IO.puts("FakeK8s: broadcasting for site #{site.internal_name}")
-
-      PubSub.broadcast(
-        :affable,
-        site.internal_name,
-        :site_ready
-      )
-    end
-  end
 end
