1a0ebfb3b7aa56da104b712589edb7684aec7201
Andrew Bruce <me@andrewbruce.net>
2021-06-24 16:18:32 +0100

    Process text as markdown, style a tiny bit.

--- web/affiliate/mix.exs
+++ web/affiliate/mix.exs
@@ -46,7 +46,8 @@ defmodule Affiliate.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:decimal, "~> 1.8"}
+      {:decimal, "~> 1.8"},
+      {:earmark, "~> 1.4.15"}
     ]
   end
 
