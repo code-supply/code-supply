d08e9b97f50d0b918cdb68a857117b74c0d52fe8
Andrew Bruce <me@andrewbruce.net>
2021-10-15 23:38:53 +0100

    Use a single, e2-standard-2 non-preemptible node.
    When committed use discount is applied, it's cheaper than 2 x e2-medium preemptibles.

--- terraform/cluster-belgium.tf
+++ terraform/cluster-belgium.tf
@@ -58,42 +58,9 @@ resource "google_container_node_pool" "waffles" {
   node_count = 1
 
   node_config {
-    preemptible  = true
     disk_size_gb = 10
 
-    machine_type = "e2-medium"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/service.management",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    tags = [var.standard_node_tag]
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-}
-
-resource "google_container_node_pool" "peas" {
-  name       = "peas"
-  location   = "europe-west1-b"
-  cluster    = google_container_cluster.belgium_pink.name
-  node_count = 1
-
-  node_locations = ["europe-west1-c"]
-
-  node_config {
-    preemptible  = true
-    disk_size_gb = 10
-
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
