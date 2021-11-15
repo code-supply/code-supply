e86dc8dd18416964fa2904172cd6a607a4b4e743
Andrew Bruce <me@andrewbruce.net>
2021-11-15 21:56:40 +0000

    Remove alert policies for now
    I don't need all those emails telling me my side project sucks.

--- terraform/site_ping/config.tf
+++ terraform/site_ping/config.tf
@@ -26,34 +26,3 @@ resource "google_monitoring_uptime_check_config" "site" {
     }
   }
 }
-
-resource "google_monitoring_alert_policy" "site" {
-  display_name = var.display_name
-
-  notification_channels = var.notification_channels
-
-  combiner = "OR"
-
-  conditions {
-    display_name = "Uptime Health Check for ${var.display_name}"
-
-    condition_threshold {
-      comparison      = "COMPARISON_GT"
-      duration        = "60s"
-      filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/check_passed\" resource.type=\"uptime_url\" metric.label.\"check_id\"=\"${google_monitoring_uptime_check_config.site.uptime_check_id}\""
-      threshold_value = 1
-
-      aggregations {
-        alignment_period     = "1200s"
-        cross_series_reducer = "REDUCE_COUNT_FALSE"
-        group_by_fields      = ["resource.*"]
-        per_series_aligner   = "ALIGN_NEXT_OLDER"
-      }
-
-      trigger {
-        count   = 0
-        percent = 100
-      }
-    }
-  }
-}
