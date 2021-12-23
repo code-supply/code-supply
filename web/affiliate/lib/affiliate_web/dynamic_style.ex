1e0ef5967b4f98f6e5c1322729f21746973df7b9
Andrew Bruce <me@andrewbruce.net>
2021-12-23 18:33:13 +0000

    Only render section styles that have been set

---
+++ web/affiliate/lib/affiliate_web/dynamic_style.ex
@@ -0,0 +1,32 @@
+defmodule AffiliateWeb.DynamicStyle do
+  @mapped_styles %{
+    "image_url" => "background-image",
+    "name" => "grid-area",
+    "text_colour" => "color",
+    "background_colour" => "background-color"
+  }
+
+  def as_list(user_values) do
+    Enum.reduce(@mapped_styles, [], fn {affable_key, css_key}, acc ->
+      case {css_key, user_values[affable_key]} do
+        {_, ""} ->
+          acc
+
+        {_, nil} ->
+          acc
+
+        {"color" = css_key, value} ->
+          ["#{css_key}:##{value}" | acc]
+
+        {"background-color" = css_key, value} ->
+          ["#{css_key}:##{value}" | acc]
+
+        {"background-image" = css_key, value} ->
+          ["#{css_key}:url(\"#{value}\")" | acc]
+
+        {_, value} ->
+          ["#{css_key}:#{value}" | acc]
+      end
+    end)
+  end
+end
---
+++ web/affiliate/test/affiliate_web/dynamic_style_test.exs
@@ -0,0 +1,29 @@
+defmodule AffiliateWeb.DynamicStyleTest do
+  use ExUnit.Case
+
+  alias AffiliateWeb.DynamicStyle
+
+  test "only renders styles for associated properties of the map" do
+    assert [
+             "background-image:url(\"http://example.com/foo.jpeg\")",
+             "background-color:#FFFF00"
+           ] ==
+             DynamicStyle.as_list(%{
+      "image_url" => "http://example.com/foo.jpeg",
+      "name" => "",
+      "text_color" => nil,
+      "background_colour" => "FFFF00"
+    })
+  end
+
+  test "colours are prefixed with hash" do
+    assert [
+             "color:#00FF00",
+             "background-color:#FFFF00"
+           ] ==
+             DynamicStyle.as_list(%{
+               "text_colour" => "00FF00",
+               "background_colour" => "FFFF00"
+             })
+  end
+end
