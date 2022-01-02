c6bfcf23035379a32da1ff7811db4b841d2bff3a
Andrew Bruce <me@andrewbruce.net>
2022-01-02 23:11:29 +0000

    Remove silly framework comment

--- web/affable/assets/src/app.ts
+++ web/affable/assets/src/app.ts
@@ -1,12 +1,3 @@
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
 import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
