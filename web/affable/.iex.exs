8a16efa87b22b8eed346f47d906718cacfba9344
Andrew Bruce <me@andrewbruce.net>
2022-01-21 12:15:47 +0000

    Load default user in iex

--- web/affable/.iex.exs
+++ web/affable/.iex.exs
@@ -1,6 +1,8 @@
+alias Affable.Accounts
 alias Affable.Repo
 alias Affable.Sites
 alias Affable.Sites.Page
 alias Affable.Sites.Site
 alias Affable.Layouts
 alias Affable.Layouts.Layout
+u = Accounts.get_user!(1)
