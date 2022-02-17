06e60e60b0600e82e1c7924899148d758b48a8e4
Andrew Bruce <me@andrewbruce.net>
2022-02-17 16:03:42 +0000

    Assign default site inside iex

--- web/affable/.iex.exs
+++ web/affable/.iex.exs
@@ -1,3 +1,4 @@
+import Ecto.Query, warn: false
 alias Affable.Accounts
 alias Affable.Repo
 alias Affable.Sites
@@ -6,3 +7,4 @@ alias Affable.Sites.Site
 alias Affable.Layouts
 alias Affable.Layouts.Layout
 u = Accounts.get_user!(1)
+s = Sites.get_site!(1)
