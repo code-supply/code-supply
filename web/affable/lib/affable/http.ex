9ff5c8f81a14cd8a60af0c1c78ca4cecfe51ce78
Andrew Bruce <me@andrewbruce.net>
2022-05-21 19:10:51 +0100

    Remove stragglers left over from separate sites

--- web/affable/lib/affable/http.ex
+++
@@ -1,6 +0,0 @@
-defmodule Affable.HTTP do
-  @callback head(String.t()) ::
-              :ok | {:error, term()}
-  @callback put(map(), String.t()) ::
-              {:ok, map()} | {:error, %{code: pos_integer(), message: String.t()}}
-end
