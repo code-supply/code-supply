042ce9835348e57b9382dd381bd8412df7af076e
Andrew Bruce <me@andrewbruce.net>
2022-06-10 21:29:35 +0100

    Try a server-side apply for operator
    This enables create-or-update functionality and lets us remove the
    static ingress definition, which is currently resetting the cert secret
    list.

--- operators/tls-lb-operator/lib/tls_lb_operator/runner.ex
+++ operators/tls-lb-operator/lib/tls_lb_operator/runner.ex
@@ -2,9 +2,9 @@ defmodule TlsLbOperator.Runner do
   @spec run({:ok, TlsLbOperator.Processor.operation()}) :: :ok
   def run({:ok, {:replace_certs, names}}) do
     {:ok, conn} = K8s.Conn.from_service_account()
-    ingress = patch_replace(names)
-    operation = K8s.Client.patch(ingress)
-    {:ok, _patched} = K8s.Client.run(conn, operation)
+    ingress = ingress(names)
+    operation = K8s.Client.apply(ingress)
+    {:ok, _} = K8s.Client.run(conn, operation)
     IO.puts("Successfully replaced certs in LB: #{inspect(names)}")
   end
 
@@ -13,11 +13,16 @@ defmodule TlsLbOperator.Runner do
     :ok
   end
 
-  defp patch_replace(secret_names) do
+  defp ingress(secret_names) do
     %{
       "apiVersion" => "networking.k8s.io/v1",
       "kind" => "Ingress",
       "metadata" => %{
+        "annotations" => %{
+          "kubernetes.io/ingress.class" => "gce",
+          "kubernetes.io/ingress.allow-http" => "false",
+          "kubernetes.io/ingress.global-static-ip-name" => "affable"
+        },
         "name" => "load-balancer-affable",
         "namespace" => "affable"
       },
@@ -25,58 +30,37 @@ defmodule TlsLbOperator.Runner do
         "tls" =>
           for name <- secret_names do
             %{"secretName" => name}
-          end
+          end,
+        "defaultBackend" => %{
+          "service" => %{
+            "name" => "affable",
+            "port" => %{
+              "number" => 80
+            }
+          }
+        },
+        "rules" => [
+          %{
+            "host" => "images.affable.app",
+            "http" => %{
+              "paths" => [
+                %{
+                  "path" => "/*",
+                  "pathType" => "ImplementationSpecific",
+                  "backend" => %{
+                    "service" => %{
+                      "name" => "imgproxy",
+                      "port" => %{
+                        "number" => 80
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          }
+        ]
       }
     }
   end
-
-  # defp base_ingress() do
-  #   %{
-  #     "apiVersion" => "networking.k8s.io/v1",
-  #     "kind" => "Ingress",
-  #     "metadata" => %{
-  #       "annotations" => %{
-  #         "kubernetes.io/ingress.class" => "gce",
-  #         "kubernetes.io/ingress.allow-http" => "false",
-  #         "kubernetes.io/ingress.global-static-ip-name" => "affable"
-  #       },
-  #       "name" => "load-balancer-affable",
-  #       "namespace" => "affable"
-  #     },
-  #     "spec" => %{
-  #       "tls" => [
-  #         %{"secretName" => "affable-www"}
-  #       ],
-  #       "defaultBackend" => %{
-  #         "service" => %{
-  #           "name" => "affable",
-  #           "port" => %{
-  #             "number" => 80
-  #           }
-  #         }
-  #       },
-  #       "rules" => [
-  #         %{
-  #           "host" => "images.affable.app",
-  #           "http" => %{
-  #             "paths" => [
-  #               %{
-  #                 "path" => "/*",
-  #                 "pathType" => "ImplementationSpecific",
-  #                 "backend" => %{
-  #                   "service" => %{
-  #                     "name" => "imgproxy",
-  #                     "port" => %{
-  #                       "number" => 80
-  #                     }
-  #                   }
-  #                 }
-  #               }
-  #             ]
-  #           }
-  #         }
-  #       ]
-  #     }
-  #   }
-  # end
 end
