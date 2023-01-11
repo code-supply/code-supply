a36e9c8a6af365e9dfdc357b72ef2a43f94e79dc
Andrew Bruce <me@andrewbruce.net>
2023-01-11 17:58:37 +0000

    Remove multi-vhost instance

--- terraform/multi-vhost.tf
+++ terraform/multi-vhost.tf
@@ -1,31 +1,3 @@
-resource "google_compute_instance" "multi-vhost" {
-  name         = "multi-vhost"
-  machine_type = "f1-micro"
-  zone         = "us-east1-b"
-  tags         = ["web", "ssh"]
-
-  boot_disk {
-    initialize_params {
-      image = "multi-vhost-1575152853"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = google_compute_address.multi-vhost.address
-    }
-  }
-
-  service_account {
-    email = google_service_account.certbot-dns.email
-    scopes = [
-      "cloud-platform",
-    ]
-  }
-}
-
 resource "google_compute_firewall" "web" {
   name    = "web"
   network = data.google_compute_network.default.name
