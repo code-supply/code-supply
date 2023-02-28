55e4a7d92763d80b3351252a3e90fff40c6b5437
Andrew Bruce <me@andrewbruce.net>
2023-02-28 23:11:27 +0000

    affable is back on the internet

--- terraform/dns-affable.tf
+++ terraform/dns-affable.tf
@@ -1,7 +1,3 @@
-locals {
-  unhinged-address = "192.168.1.182"
-}
-
 resource "google_dns_managed_zone" "affable" {
   name     = "affable"
   dns_name = "affable.app."
@@ -10,7 +6,7 @@ resource "google_dns_managed_zone" "affable" {
 resource "google_dns_record_set" "affable-root" {
   name         = google_dns_managed_zone.affable.dns_name
   managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.unhinged-address]
+  rrdatas      = [local.main-address]
   ttl          = 60
   type         = "A"
 }
@@ -18,7 +14,7 @@ resource "google_dns_record_set" "affable-root" {
 resource "google_dns_record_set" "affable-wildcard" {
   name         = "*.${google_dns_managed_zone.affable.dns_name}"
   managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.unhinged-address]
+  rrdatas      = [local.main-address]
   ttl          = 60
   type         = "A"
 }
@@ -26,7 +22,7 @@ resource "google_dns_record_set" "affable-wildcard" {
 resource "google_dns_record_set" "affable-images" {
   name         = "images.${google_dns_managed_zone.affable.dns_name}"
   managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.unhinged-address]
+  rrdatas      = [local.main-address]
   ttl          = 1800
   type         = "A"
 }
