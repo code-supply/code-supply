e9ab4503c99340d0dcdee7f3dfe4ebfe731945f9
Andrew Bruce <me@andrewbruce.net>
2023-03-01 10:01:22 +0000

    Remove affable DNS records for now
    Avoid automated signups until we care about captcha etc.

--- terraform/dns-affable.tf
+++ terraform/dns-affable.tf
@@ -2,27 +2,3 @@ resource "google_dns_managed_zone" "affable" {
   name     = "affable"
   dns_name = "affable.app."
 }
-
-resource "google_dns_record_set" "affable-root" {
-  name         = google_dns_managed_zone.affable.dns_name
-  managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.main-address]
-  ttl          = 60
-  type         = "A"
-}
-
-resource "google_dns_record_set" "affable-wildcard" {
-  name         = "*.${google_dns_managed_zone.affable.dns_name}"
-  managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.main-address]
-  ttl          = 60
-  type         = "A"
-}
-
-resource "google_dns_record_set" "affable-images" {
-  name         = "images.${google_dns_managed_zone.affable.dns_name}"
-  managed_zone = google_dns_managed_zone.affable.name
-  rrdatas      = [local.main-address]
-  ttl          = 1800
-  type         = "A"
-}
