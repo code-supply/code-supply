2b2be1b5f735c1bb66d9f04689d1f765d2444f70
Andrew Bruce <me@andrewbruce.net>
2023-03-01 10:08:06 +0000

    Remove dnsmasq config from shell.nix
    Now running a separate DNS server

--- shell.nix
+++ shell.nix
@@ -2,8 +2,7 @@ with import <nixpkgs> { };
 
 mkShell {
   packages = [
-    dnsmasq
-    elixir_1_13
+    elixir_1_14
     elixir_ls
     inotify-tools
     jq
@@ -19,9 +18,5 @@ mkShell {
     pg_ctl -D .postgres/db -l .postgres/log -o "--unix_socket_directories='$PWD/.postgres'" start
     export PGHOST="$PWD/.postgres"
     createuser affable --createdb
-    if ! pgrep dnsmasq
-    then
-      sudo dnsmasq --server='/*/8.8.8.8' --address='/*.affable.test/127.0.0.1'
-    fi
   '';
 }
