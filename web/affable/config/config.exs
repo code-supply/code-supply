b5fadf9c65c9f9cf43ca675165c2fbd5180dda14
Andrew Bruce <me@andrewbruce.net>
2023-03-05 00:44:06 +0000

    Upgrade esbuild, mark images and CSS as external.
    This stops esbuild from managing Phoenix-managed assets.
    
    See https://hexdocs.pm/phoenix/asset_management.html#images-fonts-and-external-files

--- web/affable/config/config.exs
+++ web/affable/config/config.exs
@@ -53,9 +53,10 @@ config :affable, Affable.Mailer, adapter: Bamboo.LocalAdapter
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.12.18",
+  version: "0.14.41",
   default: [
-    args: ~w(src/app.ts --bundle --target=es2016 --outdir=../priv/static/assets),
+    args:
+      ~w(src/app.ts --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
