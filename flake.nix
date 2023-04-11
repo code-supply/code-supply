f6c51650e5d409709e5a22563e193b72d2b8e1ba
Andrew Bruce <me@andrewbruce.net>
2023-04-11 11:21:31 +0100

    Build hosting as default package

--- flake.nix
+++ flake.nix
@@ -6,9 +6,18 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hostingBuild = with pkgs; with beamPackages;
+          mixRelease {
+            pname = "hosting";
+            src = ./web/hosting;
+            version = "0.0.0";
+            mixNixDeps = import ./web/hosting/deps.nix { inherit lib beamPackages; };
+          };
       in
       {
+        defaultPackage = hostingBuild;
         devShells.default = import ./shell.nix { inherit pkgs; };
       }
     );
