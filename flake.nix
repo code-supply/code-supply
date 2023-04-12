ca099a560ab4489e2f22d9d906094105cde01087
Andrew Bruce <me@andrewbruce.net>
2023-04-12 20:32:09 +0100

    Merge shell.nix into flake.nix

--- flake.nix
+++ flake.nix
@@ -49,7 +49,40 @@
           docker = dockerImageHosting;
         };
         defaultPackage = buildHosting;
-        devShells.default = import ./shell.nix { inherit pkgs; };
+        devShells.default = with pkgs;
+          mkShell {
+            packages = [
+              dnsmasq
+              elixir_1_14
+              elixir_ls
+              inotify-tools
+              jq
+              kubectl
+              kustomize
+              mix2nix
+              nodePackages."@tailwindcss/language-server"
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+              postgresql_15
+              shellcheck
+              terraform
+              terraform-lsp
+            ];
+            shellHook = ''
+              if [ ! -e .postgres ]
+              then
+                initdb -D .postgres/db
+                pg_ctl -D .postgres/db -l .postgres/log -o "--unix_socket_directories='$PWD/.postgres'" -o --max_connections=200 start
+              fi
+              export PGHOST="$PWD/.postgres"
+              createuser hosting --createdb
+              if ! pgrep dnsmasq
+              then
+                sudo dnsmasq --server='/*/8.8.8.8' --address='/*.code.test/127.0.0.1' --address '/*.code.supply/81.187.237.24'
+              fi
+            '';
+          }
+        ;
       }
     );
 }
