d611899fa34088f7dd2751afb9681599adfc1f5c
Andrew Bruce <me@andrewbruce.net>
2023-07-23 22:18:10 +0100

    Update tailwind and esbuild

--- web/hosting/config/config.exs
+++ web/hosting/config/config.exs
@@ -32,7 +32,7 @@ config :hosting, HostingWeb.Endpoint,
 config :libcluster, topologies: []
 
 config :tailwind,
-  version: "3.3.2",
+  version: "3.3.3",
   default: [
     args: ~w(
         --config=tailwind.config.js
@@ -62,7 +62,7 @@ config :hosting, Hosting.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.18.13",
+  version: "0.18.15",
   default: [
     args:
       ~w(src/app.ts --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
