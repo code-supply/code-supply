b29aed14d80197f591a24c5515b460236b75fec3
Andrew Bruce <me@andrewbruce.net>
2023-08-03 20:43:02 +0100

    Inline phoenix-utils

--- flake.nix
+++ flake.nix
@@ -1,16 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    phoenix-utils = {
-      url = "/home/andrew/workspace/phoenix-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    phoenix-utils,
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -19,11 +14,76 @@
       then self.rev
       else "dirty";
 
-    hosting = phoenix-utils.lib.buildPhoenixApp {
-      inherit pkgs system version;
-      src = ./web/hosting;
+    beamPackages = with pkgs.beam_minimal; packagesWith interpreters.erlangR25;
+    erlang = beamPackages.erlang;
+    elixir = beamPackages.elixir_1_14;
+    fetchMixDeps = beamPackages.fetchMixDeps.override {inherit elixir;};
+    mixRelease = beamPackages.mixRelease.override {inherit elixir erlang fetchMixDeps;};
+    src = ./web/hosting;
+    tailwindPath = "_build/tailwind-linux-x64";
+    esbuildPath = "_build/esbuild-linux-x64";
+
+    hosting = mixRelease {
+      inherit src version;
       pname = "hosting";
-      mix2NixOutput = ./web/hosting/deps.nix;
+      mixNixDeps = (import ./web/hosting/deps.nix) {
+        inherit beamPackages;
+        lib = pkgs.lib;
+        overrides = let
+          overrideFun = old: {
+            postInstall = ''
+              cp -v package.json "$out/lib/erlang/lib/${old.name}"
+            '';
+          };
+        in
+          _: prev: {
+            phoenix = prev.phoenix.overrideAttrs overrideFun;
+            phoenix_html = prev.phoenix_html.overrideAttrs overrideFun;
+            phoenix_live_view = prev.phoenix_live_view.overrideAttrs overrideFun;
+          };
+      };
+      postUnpack = ''
+        tailwind_version="$(${pkgs.lib.getExe elixir} ${self}/nix/extract_version.ex ${src}/config/config.exs tailwind)"
+        esbuild_version="$(${pkgs.lib.getExe elixir} ${self}/nix/extract_version.ex ${src}/config/config.exs esbuild)"
+
+        errors=0
+
+        if [[ -z "$tailwind_version" ]]
+        then
+          echo "No Tailwind version found in config/config.exs - continuing without Tailwind."
+        elif [[ "$tailwind_version" != "${pkgs.tailwindcss.version}" ]]
+        then
+          errors+=1
+          echo "error: Tailwind version mismatch: using ${pkgs.tailwindcss.version} from nix but $tailwind_version in your app!"
+        fi
+
+        if [[ -z "$esbuild_version" ]]
+        then
+          echo "No esbuild version found in config/config.exs - continuing without esbuild."
+        elif [[ "$esbuild_version" != "${pkgs.esbuild.version}" ]]
+        then
+          errors+=1
+          echo "error: esbuild version mismatch: using ${pkgs.esbuild.version} from nix but $esbuild_version in your app!"
+        fi
+
+        if [[ "$errors" > 0 ]]
+        then
+          echo "Please fix the above errors and try again."
+          exit 1
+        fi
+      '';
+
+      preBuild = ''
+        mkdir ./deps
+        cp -a _build/prod/lib/. ./deps/
+      '';
+
+      postBuild = ''
+        install ${pkgs.lib.getExe pkgs.tailwindcss} ${tailwindPath}
+        install ${pkgs.lib.getExe pkgs.esbuild} ${esbuildPath}
+
+        mix assets.deploy --no-deps-check
+      '';
     };
 
     hostingDockerImage =
@@ -32,7 +92,7 @@
         name = "codesupplydocker/hosting";
         tag = version;
         config = {
-          Cmd = ["${hosting.app}/bin/hosting" "start"];
+          Cmd = ["${pkgs.lib.getExe hosting}" "start"];
           Env = ["PATH=/bin:$PATH" "LC_ALL=C.UTF-8"];
         };
         copyToRoot = pkgs.buildEnv {
@@ -111,11 +171,16 @@
           stop
       '';
 
-    devShell = with pkgs;
-      mkShell {
-        packages = [
-          dnsmasq
+    devShell = pkgs.mkShell {
+      packages =
+        [
           dnsmasqStart
+          elixir
+          postgresStart
+          postgresStop
+        ]
+        ++ (with pkgs; [
+          dnsmasq
           elixir_ls
           google-cloud-sdk
           inotify-tools
@@ -128,23 +193,20 @@
           nodePackages.typescript
           nodePackages.typescript-language-server
           postgresql_15
-          postgresStart
-          postgresStop
           shellcheck
           terraform
           terraform-lsp
-          hosting.elixir
-        ];
-        shellHook = ''
-          export PGHOST="$(git rev-parse --show-toplevel)/.postgres"
-        '';
-      };
+        ]);
+      shellHook = ''
+        export PGHOST="$(git rev-parse --show-toplevel)/.postgres"
+      '';
+    };
   in {
     packages.${system} = {
       inherit hostingDockerImage hostingK8sManifests hostingDockerPush;
       hostingK8sDiff = hostingK8sScript "diff";
       hostingK8sApply = hostingK8sScript "apply";
-      default = hosting.app;
+      default = hosting;
     };
     devShells.${system}.default = devShell;
   };
