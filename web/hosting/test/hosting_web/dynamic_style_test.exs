a66843477d5de1003dda21e95abc03e521919f7c
Andrew Bruce <me@andrewbruce.net>
2023-09-03 12:11:18 +0100

    Swap order in an order-dependent test
    Possibly something to do with Elixir / Erlang upgrade?

--- web/hosting/test/hosting_web/dynamic_style_test.exs
+++ web/hosting/test/hosting_web/dynamic_style_test.exs
@@ -5,15 +5,15 @@ defmodule HostingWeb.DynamicStyleTest do
 
   test "only renders styles for associated properties of the map" do
     assert [
-             "background-image:url(\"http://example.com/foo.jpeg\")",
-             "background-color:#FFFF00"
+             "background-color:#FFFF00",
+             "background-image:url(\"http://example.com/foo.jpeg\")"
            ] ==
              DynamicStyle.as_list(%{
-      image_url: "http://example.com/foo.jpeg",
-      name: "",
-      text_color: nil,
-      background_colour: "FFFF00"
-    })
+               image_url: "http://example.com/foo.jpeg",
+               name: "",
+               text_color: nil,
+               background_colour: "FFFF00"
+             })
   end
 
   test "colours are prefixed with hash" do
