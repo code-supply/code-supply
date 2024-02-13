413eb842504a41df1d7570749e5cbfd0a920d51d
Andrew Bruce <me@andrewbruce.net>
2024-02-13 00:59:14 +0000

    Remove unused nix stuff

--- boxes/unhinged/github-runner.nix
+++
@@ -1,8 +0,0 @@
-{
-  services.github-runner = {
-    enable = true;
-    tokenFile = "/run/secrets/github-runner/nixos.token";
-    url = "https://github.com/code-supply/testnixrunner";
-    extraLabels = [ "nixos" ];
-  };
-}
