a70f553fd1bf09e07245c10e14951912d8626ebb
Andrew Bruce <me@andrewbruce.net>
2024-02-20 21:06:46 +0000

    Lower limit for boot entries
    Avoiding running out of /boot/efi space

--- boxes/p14s/boot.nix
+++ boxes/p14s/boot.nix
@@ -2,6 +2,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.systemd-boot.configurationLimit = 25;
 
   # boot.kernelPackages = pkgs.linuxPackages_6_1;
 
