f9e46f26d6a73b989efd8582e248d3a49b38b4a3
Andrew Bruce <me@andrewbruce.net>
2024-03-28 09:26:42 +0000

    Remove comments

--- boxes/p14s/sound.nix
+++ boxes/p14s/sound.nix
@@ -14,7 +14,6 @@
     }
   ];
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -23,11 +22,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 }
