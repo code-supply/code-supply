4791b04616fa764eeb9ae75e2cd8ddf4bb413788
Andrew Bruce <me@andrewbruce.net>
2024-04-01 18:03:07 +0100

    Separate neovim keybindings config

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -1,4 +1,5 @@
 require 'preferences'
+require 'keybindings'
 local lsp = require 'lspconfig'
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local on_attach = require 'onattach'
@@ -10,26 +11,6 @@ require 'Comment'.setup()
 
 vim.api.nvim_exec([[ autocmd vimenter * ++nested colorscheme gruvbox ]], false)
 
--- ctrl-hjkl pane movement
-vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', { noremap = true })
-
--- quick quit buffer
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<cr>', { noremap = true })
-
--- clear highlighting with space
-vim.api.nvim_set_keymap('n', '<space>', ':nohlsearch<cr>', { noremap = true })
-
--- telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Telescope grep_string<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fo', '<cmd>Telescope oldfiles<cr>', { noremap = true })
-
 -- vim-test
 vim.g["test#strategy"] = "kitty"
 vim.g["test#custom_transformations"] = {
@@ -38,16 +19,6 @@ vim.g["test#custom_transformations"] = {
   end
 }
 vim.g["test#transformation"] = "direnv"
-
-vim.api.nvim_set_keymap('n', '<F9>', ':w<cr>:TestNearest<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F12>', ':w<cr>:TestFile<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F11>', ':w<cr>:TestNearest<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F10>', ':w<cr>:TestLast<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F8>', ':w<cr>:TestVisit<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<F7>', ':w<cr>:TestSuite<cr>', { noremap = true })
-
--- sort lists inside []
-vim.api.nvim_set_keymap('n', '<F5>', '!i[sort<cr>', { noremap = true })
 
 lsp.lua_ls.setup {
   on_attach = on_attach,
