7ceeabefc4d040bf9f5a98a63a3e555d588a9d21
Andrew Bruce <me@andrewbruce.net>
2024-04-01 22:17:10 +0100

    Shuffle init.lua

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -1,9 +1,10 @@
-require 'preferences'
-require 'keybindings'
-require 'appearance'
 local lsp = require 'lspconfig'
 local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
 local on_attach = require 'onattach'
+
+require 'preferences'
+require 'keybindings'
+require 'appearance'
 require 'completion'
 require 'fidgetconfig'
 require 'treesitter'
@@ -16,7 +17,6 @@ require 'nixconfig'.setup(lsp, capabilities, on_attach)
 require 'rustconfig'.setup(lsp, capabilities, on_attach)
 
 lsp.idris2_lsp.setup {}
-
 lsp.terraform_lsp.setup {}
 vim.g.terraform_fmt_on_save = "1"
 
