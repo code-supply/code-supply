5fec77ca836da2873fd18a43bda05614eb95eb1a
Andrew Bruce <me@andrewbruce.net>
2024-04-01 19:36:08 +0100

    Separate neovim lua module

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -7,6 +7,7 @@ require 'completion'
 require 'fidgetconfig'
 require 'treesitter'
 require 'elixirconfig'.setup(capabilities, on_attach)
+require 'luaconfig'.setup(lsp, capabilities, on_attach)
 require 'Comment'.setup()
 
 vim.api.nvim_exec([[ autocmd vimenter * ++nested colorscheme gruvbox ]], false)
@@ -19,40 +20,6 @@ vim.g["test#custom_transformations"] = {
   end
 }
 vim.g["test#transformation"] = "direnv"
-
-lsp.lua_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-
-  on_init = function(client)
-    local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using
-            -- (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT'
-          },
-          -- Make the server aware of Neovim runtime files
-          workspace = {
-            checkThirdParty = false,
-            library = {
-              vim.env.VIMRUNTIME
-              -- "${3rd}/luv/library"
-              -- "${3rd}/busted/library",
-            }
-            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            -- library = vim.api.nvim_get_runtime_file("", true)
-          }
-        }
-      })
-
-      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-    end
-    return true
-  end
-}
 
 lsp.tsserver.setup {
   on_attach = on_attach,
