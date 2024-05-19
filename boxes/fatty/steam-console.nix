8bec24b80eff58d03495ff0e354cf3aa3de549c6
Andrew Bruce <me@andrewbruce.net>
2024-05-19 12:45:33 +0100

    Full-screen every window for openbox

--- boxes/fatty/steam-console.nix
+++ boxes/fatty/steam-console.nix
@@ -5,9 +5,14 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.windowManager.openbox.enable = true;
 
+  programs.steam.enable = true;
+
   environment.etc = {
     "xdg/autostart/steam.desktop" = {
       source = "${pkgs.steam}/share/applications/steam.desktop";
+    };
+    "xdg/openbox/rc.xml" = {
+      source = ./openbox-rc.xml;
     };
     "xdg/openbox/menu.xml" = {
       source = ./openbox-menu.xml;
