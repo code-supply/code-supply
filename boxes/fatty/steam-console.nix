5913357f9768960f433e84d50c0f761db86f53f2
Andrew Bruce <me@andrewbruce.net>
2024-05-19 12:46:13 +0100

    Revert openbox stuff
    Don't fancy reimplementing everything, thanks.

--- boxes/fatty/steam-console.nix
+++ boxes/fatty/steam-console.nix
@@ -1,21 +1,7 @@
 { pkgs, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.windowManager.openbox.enable = true;
-
-  programs.steam.enable = true;
-
-  environment.etc = {
-    "xdg/autostart/steam.desktop" = {
-      source = "${pkgs.steam}/share/applications/steam.desktop";
-    };
-    "xdg/openbox/rc.xml" = {
-      source = ./openbox-rc.xml;
-    };
-    "xdg/openbox/menu.xml" = {
-      source = ./openbox-menu.xml;
-    };
+  environment.etc."xdg/autostart/steam.desktop" = {
+    source = "${pkgs.steam}/share/applications/steam.desktop";
   };
 }
