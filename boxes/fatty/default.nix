8189ab5812f20fadd070e54bdcbbb3538b3e6566
Andrew Bruce <me@andrewbruce.net>
2024-05-19 11:57:18 +0100

    Fatty game adjustments
    - No more joycons: using Xbox controllers now
    - Try Openbox to avoid gnome's bloat including mining daemon etc.

--- boxes/fatty/default.nix
+++ boxes/fatty/default.nix
@@ -3,14 +3,11 @@
 nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
-    ../common/gui.nix
     ../common/locale.nix
     ../common/nix.nix
-    ../common/steam.nix
     ../common/user.nix
     ./configuration.nix
     ./hardware-configuration.nix
-    ./joycon.nix
     ({ nix.package = nix; })
     ./nvidia.nix
     ./steam-console.nix
--- boxes/fatty/steam-console.nix
+++ boxes/fatty/steam-console.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 {
-  environment.etc."xdg/autostart/steam.desktop" = {
-    source = "${pkgs.steam}/share/applications/steam.desktop";
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.windowManager.openbox.enable = true;
+
+  environment.etc = {
+    "xdg/autostart/steam.desktop" = {
+      source = "${pkgs.steam}/share/applications/steam.desktop";
+    };
+    "xdg/openbox/menu.xml" = {
+      source = ./openbox-menu.xml;
+    };
   };
 }
