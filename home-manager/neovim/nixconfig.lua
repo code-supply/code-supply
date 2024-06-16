41b9de6926b7b429660a149b87a8a12fb38b8f0f
Andrew Bruce <me@andrewbruce.net>
2024-06-16 13:09:05 +0100

    Neovim: commentstring for nix files

--- home-manager/neovim/nixconfig.lua
+++ home-manager/neovim/nixconfig.lua
@@ -1,6 +1,9 @@
 return {
   setup = function(lsp, capabilities)
     lsp.nil_ls.setup {
+      on_attach = function(_client)
+        vim.bo.commentstring = "# %s"
+      end,
       settings = {
         ["nil"] = {
           formatting = {
