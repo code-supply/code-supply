5667ab0b72c96154c2128c6b0d99f9b431aa6ac5
Andrew Bruce <me@andrewbruce.net>
2024-06-21 00:37:26 +0100

    DrivenByMoss

--- home-manager/audio-programs.nix
+++ home-manager/audio-programs.nix
@@ -25,24 +25,61 @@
   };
 
   home = {
-    file = {
-      pipewire-config =
-        let
-          json = pkgs.formats.json { };
-        in
-        {
-          target = ".config/pipewire/pipewire.conf.d/92-low-latency.conf";
-          source = json.generate "92-low-latency.conf" {
-            context.properties = {
-              default.clock.rate = 48000;
-              default.clock.quantum = 16;
-              default.clock.min-quantum = 16;
-              default.clock.max-quantum = 16;
-            };
+    file =
+      let
+        # give up and just download the thing with fetchzip?
+        drivenByMoss =
+          pkgs.fetchzip {
+            url = "https://www.mossgrabers.de/Software/Bitwig/DrivenByMoss-23.2.2-Bitwig.zip";
+            hash = "sha256-+9EKyeYgGKVIt+34Dgcz2xK4sZ7w72424RLspB5+05Q=";
+            stripRoot = false;
           };
+        # drivenByMoss = with pkgs; (
+        #   let pname = "DrivenByMoss"; version = "23.2.2"; in maven.buildMavenPackage {
+        #     inherit pname version;
 
-        };
-    };
+        #     src = fetchFromGitHub {
+        #       owner = "git-moss";
+        #       repo = pname;
+        #       rev = version;
+        #       hash = "sha256-/u9Nh9TAQ8sj/nJkB0qmgUqUGEAISxyL2+8kPtMT3RE=";
+        #     };
+
+        #     mvnHash = "sha256-pzzpNWH6T03KoquaTTWQGGnMbj4yKhmBCM9ljhB+71E=";
+        #     mvnParameters = "-Dbitwig.extension.directory=target";
+
+        #     nativeBuildInputs = [ unzip ];
+
+        #     installPhase = ''
+        #       mkdir $out
+        #       cp target/DrivenByMoss-23.2.2-Bitwig.zip $out/DrivenByMoss.bwextension
+        #     '';
+        #   }
+        # );
+      in
+      {
+        pipewire-config =
+          let
+            json = pkgs.formats.json { };
+          in
+          {
+            target = ".config/pipewire/pipewire.conf.d/92-low-latency.conf";
+            source = json.generate "92-low-latency.conf" {
+              context.properties = {
+                default.clock.rate = 48000;
+                default.clock.quantum = 16;
+                default.clock.min-quantum = 16;
+                default.clock.max-quantum = 16;
+              };
+            };
+
+          };
+        drivenByMossBitwigExtension =
+          {
+            target = "Bitwig Studio/Extensions/DrivenByMoss.bwextension";
+            source = "${drivenByMoss}/DrivenByMoss.bwextension";
+          };
+      };
 
     packages = with pkgs; [
       abcde
