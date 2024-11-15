26b796773a9133757f3ddaeb513241708be26d73
Andrew Bruce <me@andrewbruce.net>
2024-11-15 18:01:09 +0000

    Whoops

--- boxes/fatty/configuration.nix
+++ boxes/fatty/configuration.nix
@@ -10,7 +10,7 @@
 
   services.printing.enable = true;
 
-  iare.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
