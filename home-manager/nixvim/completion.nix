050d39a1fbb73667aab8dc9cc0351560cbd44af3
Andrew Bruce <me@andrewbruce.net>
2024-11-23 23:53:39 +0000

    Add luasnip to make completion work properly

--- home-manager/nixvim/completion.nix
+++ home-manager/nixvim/completion.nix
@@ -1,18 +1,21 @@
 {
-  programs.nixvim.plugins.cmp = {
-    enable = true;
-    settings = {
-      sources = [
-        { name = "buffer"; }
-        { name = "luasnip"; }
-        { name = "nvim_lsp"; }
-        { name = "path"; }
-      ];
-      snippet.expand = ''
-        function(args)
-          require('luasnip').lsp_expand(args.body)
-        end
-      '';
+  programs.nixvim.plugins = {
+    cmp = {
+      enable = true;
+      settings = {
+        sources = [
+          { name = "buffer"; }
+          { name = "luasnip"; }
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+        ];
+        snippet.expand = ''
+          function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        '';
+      };
     };
+    luasnip.enable = true;
   };
 }
