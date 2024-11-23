ef0144a707688f7b4d88423242416dbc1e322d7e
Andrew Bruce <me@andrewbruce.net>
2024-11-23 15:01:16 +0000

    Vim organisation

---
+++ home-manager/nixvim/completion.nix
@@ -0,0 +1,18 @@
+{
+  programs.nixvim.plugins.cmp = {
+    enable = true;
+    settings = {
+      sources = [
+        { name = "buffer"; }
+        { name = "luasnip"; }
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+      ];
+      snippet.expand = ''
+        function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      '';
+    };
+  };
+}
--- home-manager/nixvim/default.nix
+++ home-manager/nixvim/default.nix
@@ -2,6 +2,8 @@
 
 {
   imports = [
+    ./completion.nix
+    ./format-on-save.nix
     ./keymaps.nix
     ./lsp.nix
     ./treesitter.nix
@@ -15,23 +17,6 @@
       fidget.enable = true;
       telescope.enable = true;
       web-devicons.enable = true;
-
-      cmp = {
-        enable = true;
-        settings = {
-          sources = [
-            { name = "buffer"; }
-            { name = "luasnip"; }
-            { name = "nvim_lsp"; }
-            { name = "path"; }
-          ];
-          snippet.expand = ''
-            function(args)
-              require('luasnip').lsp_expand(args.body)
-            end
-          '';
-        };
-      };
     };
 
     extraPlugins = with pkgs.vimPlugins; [
@@ -46,14 +31,5 @@
       expandtab = true;
       wrap = false;
     };
-
-    autoCmd = [
-      {
-        event = [ "BufWritePre" ];
-        callback = {
-          __raw = "function() vim.lsp.buf.format({ async = false }) end";
-        };
-      }
-    ];
   };
 }
