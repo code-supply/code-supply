cbac2e8c90b7dc378ad3ea128f3493b492c2a1f4
Andrew Bruce <me@andrewbruce.net>
2024-12-14 07:42:40 +0000

    Remove non-nixvim neovim config

--- home-manager/neovim/vimtest.lua
+++
@@ -1,6 +0,0 @@
-vim.g["test#custom_transformations"] = {
-  direnv = function(cmd)
-    return 'direnv exec "$(git rev-parse --show-toplevel)" ' .. cmd
-  end
-}
-vim.g["test#transformation"] = "direnv"
