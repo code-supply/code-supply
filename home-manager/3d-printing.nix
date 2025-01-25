46a28a7b099a07712c85309b8a62daf45ac81883
Andrew Bruce <me@andrewbruce.net>
2025-01-25 22:09:54 +0000

    OpenSCAD with latest LSP

--- home-manager/3d-printing.nix
+++ home-manager/3d-printing.nix
@@ -1,10 +1,40 @@
 { pkgs, ... }:
 
+with pkgs;
+
 {
-  home.packages = with pkgs; [
+  home.packages = [
     appimage-run
     blender
     cura-appimage
+    openscad-unstable
     prusa-slicer
   ];
+
+  programs.nixvim.plugins.lsp.servers.openscad_lsp = {
+    enable = true;
+    package = openscad-lsp.override (
+      let
+        rp = rustPlatform;
+      in
+      {
+        rustPlatform = rp // {
+          buildRustPackage =
+            args:
+            rp.buildRustPackage (
+              args
+              // {
+                src = fetchFromGitHub {
+                  owner = "Leathong";
+                  repo = "openscad-LSP";
+                  rev = "d6501c05a890f576c1f25c6aa2868cbada4a1d6e";
+                  hash = "sha256-BNHoPMWjZC0dtD9/OUvgx/WPzqAOhhmXVkutCG2SybA=";
+                };
+                cargoHash = "sha256-JaX/BokVeHcD/38zbUFYucAqpASSxV9gvvjYvjX7xdA=";
+              }
+            );
+        };
+      }
+    );
+  };
 }
