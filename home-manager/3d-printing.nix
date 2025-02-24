1f27da5c401388bb833e645b10a979254150abe7
Andrew Bruce <me@andrewbruce.net>
2025-02-24 19:59:25 +0000

    Remove custom openscad-lsp which broke

--- home-manager/3d-printing.nix
+++ home-manager/3d-printing.nix
@@ -26,29 +26,6 @@ with pkgs;
     ];
     plugins.lsp.servers.openscad_lsp = {
       enable = true;
-      package = openscad-lsp.override (
-        let
-          rp = rustPlatform;
-        in
-        {
-          rustPlatform = rp // {
-            buildRustPackage =
-              args:
-              rp.buildRustPackage (
-                args
-                // {
-                  src = fetchFromGitHub {
-                    owner = "Leathong";
-                    repo = "openscad-LSP";
-                    rev = "d6501c05a890f576c1f25c6aa2868cbada4a1d6e";
-                    hash = "sha256-BNHoPMWjZC0dtD9/OUvgx/WPzqAOhhmXVkutCG2SybA=";
-                  };
-                  cargoHash = "sha256-JaX/BokVeHcD/38zbUFYucAqpASSxV9gvvjYvjX7xdA=";
-                }
-              );
-          };
-        }
-      );
     };
   };
 }
