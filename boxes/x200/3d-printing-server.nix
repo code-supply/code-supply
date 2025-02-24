4cb0620dd22125ce0832d0dc5d71d362e98e7db5
Andrew Bruce <me@andrewbruce.net>
2025-02-24 21:48:12 +0000

    Consolidate Klipper config into files.

--- boxes/x200/3d-printing-server.nix
+++ boxes/x200/3d-printing-server.nix
@@ -1,69 +1,20 @@
 {
   pkgs,
   config,
-  shubham-klipper,
   ...
 }:
 
 let
-  mesh = ''
-    [bltouch]
-    z_offset: 0.303
-
-    [bed_mesh default]
-    version: 1
-    points: 
-      0.066250, -0.003750, -0.011250, 0.022500, 0.186250
-      0.123750, 0.028750, -0.011250, -0.011250, 0.103750
-      0.151250, 0.022500, -0.035000, -0.071250, 0.012500
-      0.165000, 0.020000, -0.078750, -0.147500, -0.083750
-      0.168750, 0.011250, -0.105000, -0.193750, -0.195000
-    x_count: 5
-    y_count: 5
-    mesh_x_pps: 2
-    mesh_y_pps: 2
-    algo: bicubic
-    tension: 0.2
-    min_x: 10.0
-    max_x: 206.0
-    min_y: 10.0
-    max_y: 210.48
-  '';
-
-  klipper_config = ''
-    [virtual_sdcard]
-    path: /var/lib/moonraker/gcodes
-
-    [pause_resume]
-    [display_status]
-
-    [gcode_macro CANCEL_PRINT]
-    description: Cancel the actual running print
-    rename_existing: CANCEL_PRINT_BASE
-    gcode:
-      TURN_OFF_HEATERS
-      CANCEL_PRINT_BASE
-  '';
-  printerConfig = pkgs.runCommand "create-printer-config" { } ''
+  configs = pkgs.runCommand "create-printer-config" { } ''
     mkdir $out
-
-    sed "/fluidd.cfg/d;s#macros.cfg#$out/macros.cfg#;s/mcu_temp/mcu/;s/raspberry_pi/host/" \
-      < ${shubham-klipper}/printer.cfg \
-      > $out/printer.cfg
-
-    cat <<EOF >> $out/printer.cfg
-    ${klipper_config}
-    ${mesh}
-    EOF
-
-    cp ${shubham-klipper}/macros.cfg $out/
+    sed 's#\[include \(.*\)\]#[include ${./klipper}/\1]#' < ${./klipper/printer.cfg} > $out/printer.cfg
   '';
 in
 
 {
   services.klipper = {
     enable = true;
-    configFile = "${printerConfig}/printer.cfg";
+    configFile = "${configs}/printer.cfg";
     logFile = "/var/lib/klipper/klipper.log";
     user = "klipper";
     group = "klipper";
