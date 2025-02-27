0bdffd8ddc2f89d7072b8f3daffacd6964c69891
Andrew Bruce <me@andrewbruce.net>
2025-02-27 20:58:20 +0000

    Use existing option instead of reimplementing

--- boxes/x200/3d-printing-server.nix
+++ boxes/x200/3d-printing-server.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  config,
   ...
 }:
 
@@ -20,33 +19,6 @@ in
     group = "klipper";
   };
 
-  security.polkit = {
-    enable = true;
-    # https://github.com/Arksine/moonraker/blob/master/scripts/set-policykit-rules.sh
-    extraConfig = ''
-      polkit.addRule(function(action, subject) {
-          if ((action.id == "org.freedesktop.systemd1.manage-units" ||
-               action.id == "org.freedesktop.login1.power-off" ||
-               action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
-               action.id == "org.freedesktop.login1.reboot" ||
-               action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-               action.id == "org.freedesktop.login1.halt" ||
-               action.id == "org.freedesktop.login1.halt-multiple-sessions" ||
-               action.id.startsWith("org.freedesktop.packagekit.")) &&
-              subject.user == "${config.users.users.moonraker.name}") {
-              var regex = "^Groups:.+${toString config.ids.gids.moonraker}";
-              var cmdpath = "/proc/" + subject.pid.toString() + "/status";
-              try {
-                  polkit.spawn(["grep", "-Po", regex, cmdpath]);
-                  return polkit.Result.YES;
-              } catch (error) {
-                  return polkit.Result.NOT_HANDLED;
-              }
-          }
-      });
-    '';
-  };
-
   users = {
     users = {
       klipper = {
@@ -58,9 +30,12 @@ in
     groups.klipper = { };
   };
 
+  security.polkit.enable = true;
+
   services.moonraker = {
     enable = true;
     address = "0.0.0.0";
+    allowSystemControl = true;
     settings = {
       octoprint_compat = { };
       authorization = {
