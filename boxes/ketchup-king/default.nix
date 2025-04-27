1cd00d9adfa8b884defddc8e6c3638b457db744b
Andrew Bruce <me@andrewbruce.net>
2025-04-27 22:31:31 +0100

    Shuffle Ketchup's default.nix around

--- boxes/ketchup-king/default.nix
+++ boxes/ketchup-king/default.nix
@@ -7,19 +7,15 @@
 {
   system = "aarch64-linux";
   modules = [
+    "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
     ./3d-printing-server.nix
     ../common/3d-printing-server.nix
     ../common/nix.nix
     ../common/ssh.nix
     ../common/user.nix
     ./network.nix
-    ./rpi.nix
     nixos-hardware.nixosModules.raspberry-pi-4
-    {
-      system.stateVersion = "25.05";
-      imports = [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-      ];
-    }
+    ./rpi.nix
+    { system.stateVersion = "25.05"; }
   ];
 }
