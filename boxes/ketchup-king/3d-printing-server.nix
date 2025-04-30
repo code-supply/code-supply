3051e6778f6da73f87392711d419df9440479952
Andrew Bruce <me@andrewbruce.net>
2025-04-30 16:43:16 +0100

    Show Voron logo on boot

--- boxes/ketchup-king/3d-printing-server.nix
+++ boxes/ketchup-king/3d-printing-server.nix
@@ -19,6 +19,10 @@ in
   boot = {
     plymouth = {
       enable = true;
+      logo = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/VoronDesign/Voron-Documentation/a74b763bb5eaa93d1e566d662de9f4d01caf44a1/images/voron_design_logo.png";
+        hash = "sha256-6rWw6pX4VnPdNW9Xa9bZed9AllClfFO3RU0xTGZRmvY=";
+      };
     };
     consoleLogLevel = 3;
     initrd.verbose = false;
