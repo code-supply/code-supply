73ae7d11b041e2046884f3deaa29a8d78fec541a
Andrew Bruce <me@andrewbruce.net>
2025-04-30 16:17:24 +0100

    Plymouth for Ketchup

--- boxes/ketchup-king/3d-printing-server.nix
+++ boxes/ketchup-king/3d-printing-server.nix
@@ -16,6 +16,22 @@ in
     xdotool
   ];
 
+  boot = {
+    plymouth = {
+      enable = true;
+    };
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    loader.timeout = 0;
+  };
+
   services.xserver = {
     enable = true;
 
