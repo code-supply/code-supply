7fc5a5acf3574bb3eacec37c4205af7c4f64544b
Andrew Bruce <me@andrewbruce.net>
2025-06-15 17:53:30 +0100

    Extract Plymouth to klix

--- boxes/ketchup-king/3d-printing-server.nix
+++ boxes/ketchup-king/3d-printing-server.nix
@@ -19,16 +19,6 @@ in
     lsof
   ];
 
-  boot = {
-    plymouth = {
-      enable = true;
-      logo = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/VoronDesign/Voron-Documentation/a74b763bb5eaa93d1e566d662de9f4d01caf44a1/images/voron_design_logo.png";
-        hash = "sha256-6rWw6pX4VnPdNW9Xa9bZed9AllClfFO3RU0xTGZRmvY=";
-      };
-    };
-  };
-
   programs.firefox.enable = false;
   programs.firefox.preferences = {
     "layout.css.devPixelsPerPx" = "1.5";
