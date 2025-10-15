e5d52de882a5d9c949ea9ea38b741e7c543974a8
Andrew Bruce <me@andrewbruce.net>
2025-10-15 21:41:42 +0100

    Working Raspberry Pi 5 base image

--- flake.nix
+++ flake.nix
@@ -24,11 +24,23 @@
       url = "github:code-supply/klix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-raspberrypi = {
+      url = "github:nvmd/nixos-raspberrypi/main";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
 
   outputs =
@@ -40,9 +52,10 @@
       home-manager,
       isd,
       klix,
+      nixos-raspberrypi,
       nixpkgs,
       nixvim,
-    }:
+    }@inputs:
     let
       system = "x86_64-linux";
       version = if self ? rev then self.rev else "dirty";
@@ -101,6 +114,55 @@
       packages.aarch64-linux.ketchupKingSDCard =
         self.nixosConfigurations.ketchup-king.config.system.build.sdImage;
 
+      packages.aarch64-linux.rp5SDCard =
+        let
+          installer = nixos-raspberrypi.lib.nixosInstaller {
+            specialArgs = inputs;
+            modules = [
+              nixos-raspberrypi.inputs.nixos-images.nixosModules.sdimage-installer
+
+              (
+                {
+                  config,
+                  lib,
+                  modulesPath,
+                  ...
+                }:
+                {
+                  disabledModules = [
+                    # disable the sd-image module that nixos-images uses
+                    (modulesPath + "/installer/sd-card/sd-image-aarch64-installer.nix")
+                  ];
+                  # nixos-images sets this with `mkForce`, thus `mkOverride 40`
+                  image.baseName =
+                    let
+                      cfg = config.boot.loader.raspberryPi;
+                    in
+                    lib.mkOverride 40 "nixos-installer-rpi${cfg.variant}-${cfg.bootloader}";
+                }
+              )
+
+              {
+                imports = with nixos-raspberrypi.nixosModules; [
+                  raspberry-pi-5.base
+                  raspberry-pi-5.page-size-16k
+                ];
+              }
+
+              {
+                networking.hostName = "rp5";
+                users.users.nixos.openssh.authorizedKeys.keys = [
+                  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvEdU8Vs+25y3uN6YTFqNPKGdr7Z+v6lhuMQ0ppJ33pWPUh/AMMtumEr1Jb6+oAN7q4fozbu6o+9U1BlD0VXeIIAKaekru0tFzhcrvfQO8oiLs4f2TaQW8w5aprjmK8k5ZWdD2PV03jzxXnMhmFANr+zPgxLgy+J9JkoQJUcDBic1C1nbXLgHl7D0027aBT1NBGtK8ildCiDHmEh8qlCVJI6CSCS6fesZiHiyuEIVF1BG/DR9PWganyyuCHEav11fmiWiJAMUfCNwWosEoT4w0CTJ3vIhqeF9uAilo/NdUBGWJF/hLjWVFVoJ8uYjQyA70d0PY6mZjJgv+MUxsJoxYY1mQ+QqoIp3gF2/XAX1LwZPgd3Qh+cO2hkvBQ82g2TXqzu3bTSr/Gf4lUSmGPsozFhvkuRLL78wLefpY333NJ+ysp2XMwDDH0LEdQxeRbjlItpE7yEADiwe92RvsxxWgTFpHzMbxGaC95B0ZA2PUjY1izJQMPvGkV/4mx3QtC8wW/KeJJ52aWEO/Lcaec69bkKh56bOekipu6Jgs30e7CPtcgnMfllZRYbXvv05MlSKSTycgEMVssmjGEpKtVfIJiQUAhML4tQxfhZqy2t1/61tO2FgLDytoxLojQzJz9VOlsGcK7butCSJx3wrgCvU2Yc5fD3mskFJUJO+jFFqyMQ== cardno:16_019_463"
+                ];
+                users.users.root.openssh.authorizedKeys.keys = [
+                  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvEdU8Vs+25y3uN6YTFqNPKGdr7Z+v6lhuMQ0ppJ33pWPUh/AMMtumEr1Jb6+oAN7q4fozbu6o+9U1BlD0VXeIIAKaekru0tFzhcrvfQO8oiLs4f2TaQW8w5aprjmK8k5ZWdD2PV03jzxXnMhmFANr+zPgxLgy+J9JkoQJUcDBic1C1nbXLgHl7D0027aBT1NBGtK8ildCiDHmEh8qlCVJI6CSCS6fesZiHiyuEIVF1BG/DR9PWganyyuCHEav11fmiWiJAMUfCNwWosEoT4w0CTJ3vIhqeF9uAilo/NdUBGWJF/hLjWVFVoJ8uYjQyA70d0PY6mZjJgv+MUxsJoxYY1mQ+QqoIp3gF2/XAX1LwZPgd3Qh+cO2hkvBQ82g2TXqzu3bTSr/Gf4lUSmGPsozFhvkuRLL78wLefpY333NJ+ysp2XMwDDH0LEdQxeRbjlItpE7yEADiwe92RvsxxWgTFpHzMbxGaC95B0ZA2PUjY1izJQMPvGkV/4mx3QtC8wW/KeJJ52aWEO/Lcaec69bkKh56bOekipu6Jgs30e7CPtcgnMfllZRYbXvv05MlSKSTycgEMVssmjGEpKtVfIJiQUAhML4tQxfhZqy2t1/61tO2FgLDytoxLojQzJz9VOlsGcK7butCSJx3wrgCvU2Yc5fD3mskFJUJO+jFFqyMQ== cardno:16_019_463"
+                ];
+              }
+            ];
+          };
+        in
+        installer.config.system.build.sdImage;
+
       devShells.${system}.default = devShell;
 
       homeConfigurations."andrew@fatty" = import ./home-manager/fatty.nix {
@@ -140,6 +202,19 @@
               ;
           }
         );
+
+        rp5 = nixos-raspberrypi.lib.nixosSystem {
+          modules = [
+            {
+              imports = with nixos-raspberrypi.nixosModules; [
+                raspberry-pi-5.base
+                raspberry-pi-5.page-size-16k
+              ];
+            }
+
+            { networking.hostName = "rp5"; }
+          ];
+        };
       };
 
       templates.elixir = {
