f5620b475652f8ff21b19fddba0f795667e9f7b6
Andrew Bruce <me@andrewbruce.net>
2025-11-08 16:33:29 +0000

    Revert "KDE"
    This reverts commit 30f6a79e0842dc6a6891648887374490c3fc2da6.

--- boxes/common/gui.nix
+++ boxes/common/gui.nix
@@ -1,6 +1,5 @@
-{ lib, ... }:
 {
   services.xserver.enable = true;
-  services.displayManager.gdm.enable = lib.mkDefault true;
+  services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 }
