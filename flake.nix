2498e0a04cbef62980948ae7db17b8e99c85c882
Andrew Bruce <me@andrewbruce.net>
2025-11-09 14:35:04 +0000

    Add plugins and klipperscreen to rp5

--- flake.nix
+++ flake.nix
@@ -154,14 +154,24 @@
             {
               imports = klix.lib.machineImports.raspberry-pi-5;
 
-              services.klix = {
-                configDir = ./boxes/ketchup-king/klipper;
-              };
-
               networking.hostName = "rp5";
               users.users.root.openssh.authorizedKeys.keys = [
                 "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvEdU8Vs+25y3uN6YTFqNPKGdr7Z+v6lhuMQ0ppJ33pWPUh/AMMtumEr1Jb6+oAN7q4fozbu6o+9U1BlD0VXeIIAKaekru0tFzhcrvfQO8oiLs4f2TaQW8w5aprjmK8k5ZWdD2PV03jzxXnMhmFANr+zPgxLgy+J9JkoQJUcDBic1C1nbXLgHl7D0027aBT1NBGtK8ildCiDHmEh8qlCVJI6CSCS6fesZiHiyuEIVF1BG/DR9PWganyyuCHEav11fmiWiJAMUfCNwWosEoT4w0CTJ3vIhqeF9uAilo/NdUBGWJF/hLjWVFVoJ8uYjQyA70d0PY6mZjJgv+MUxsJoxYY1mQ+QqoIp3gF2/XAX1LwZPgd3Qh+cO2hkvBQ82g2TXqzu3bTSr/Gf4lUSmGPsozFhvkuRLL78wLefpY333NJ+ysp2XMwDDH0LEdQxeRbjlItpE7yEADiwe92RvsxxWgTFpHzMbxGaC95B0ZA2PUjY1izJQMPvGkV/4mx3QtC8wW/KeJJ52aWEO/Lcaec69bkKh56bOekipu6Jgs30e7CPtcgnMfllZRYbXvv05MlSKSTycgEMVssmjGEpKtVfIJiQUAhML4tQxfhZqy2t1/61tO2FgLDytoxLojQzJz9VOlsGcK7butCSJx3wrgCvU2Yc5fD3mskFJUJO+jFFqyMQ== cardno:16_019_463"
               ];
+
+              services.klix = {
+                configDir = ./boxes/ketchup-king/klipper;
+              };
+
+              services.klipper = {
+                plugins = {
+                  kamp.enable = true;
+                  shaketune.enable = true;
+                  z_calibration.enable = true;
+                };
+              };
+
+              services.klipperscreen.enable = true;
             }
           ];
         };
