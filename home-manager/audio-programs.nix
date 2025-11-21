49d63f5e28ceab069f153a2e65d2d3ec173bc348
Andrew Bruce <me@andrewbruce.net>
2025-11-21 10:43:35 +0000

    xwax: upgrade, patch and configure
    Use beta
    Drop ALSA
    Relative timecode for Phase DJ

--- home-manager/audio-programs.nix
+++ home-manager/audio-programs.nix
@@ -19,47 +19,93 @@
         };
     };
 
-    packages = with pkgs; [
-      (writeShellApplication {
-        runtimeInputs = [
-          xwax
-        ];
-        name = "xwax-run";
-        text = ''
-          set -m
+    packages =
+      with pkgs;
+      let
+        xwax-beta = xwax.overrideAttrs (
+          finalAttrs: previousAttrs: {
+            version = "1.10-beta1";
 
-          xwax -l ~/Music -j left -j right -g /2 -c -k &
+            src = fetchurl {
+              url = "https://xwax.org/releases/xwax-${finalAttrs.version}.tar.gz";
+              hash = "sha256-zJHkKhWXCKGhAYalUArYC6sLFX7NtWTu4yGY+60aB40=";
+            };
 
-          until pw-link -i | grep -q xwax
-          do
-            echo "Waiting for xwax..."
-            sleep 0.1
-          done
+            patches = [
+              ./xwax-relative.patch
+            ];
 
-          pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX0 xwax:left_timecode_L
-          pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX1 xwax:left_timecode_R
+            postPatch = ''
+              # font loading in xwax is relying on a hardcoded list of paths,
+              # therefore we patch interface.c to also look up in the dejavu_fonts nix store path
+              substituteInPlace interface.c \
+                --replace-fail "/usr/X11R6/lib/X11/fonts/TTF" "${dejavu_fonts.outPath}/share/fonts/truetype/"
 
-          pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX6 xwax:right_timecode_L
-          pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX7 xwax:right_timecode_R
+              # make paths to executed binaries hermetic:
+              substituteInPlace import \
+                --replace-fail "exec cdparanoia" "exec ${lib.getExe cdparanoia}" \
+                --replace-fail "exec ffmpeg" "exec ${lib.getExe ffmpeg}"
+            '';
 
-          pw-link xwax:left_playback_L alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX2
-          pw-link xwax:left_playback_R alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX3
+            buildInputs = previousAttrs.buildInputs ++ [
+              SDL2.dev
+              SDL2_ttf
+            ];
 
-          pw-link xwax:right_playback_L alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX4
-          pw-link xwax:right_playback_R alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX5
+            configureFlags = [
+              "--enable-jack"
+            ];
+          }
+        );
+      in
+      [
+        (writeShellApplication {
+          runtimeInputs = [
+            xwax-beta
+          ];
+          name = "xwax-run";
+          text = ''
+            set -m
 
-          fg
-        '';
-      })
-      abcde
-      alsa-scarlett-gui
-      audacity
-      bitwig-studio5
-      elektroid
-      mixxx
-      pipecontrol
-      qpwgraph
-      xwax
-    ];
+            xwax \
+              --crate ~/Music \
+              --timecode serato_2a_relative \
+              --jack left \
+              --jack right \
+              --geometry /2 \
+              --protect \
+              &
+
+            until pw-link -i | grep -q xwax
+            do
+              echo "Waiting for xwax..."
+              sleep 0.1
+            done
+
+            pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX0 xwax:left_timecode_L
+            pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX1 xwax:left_timecode_R
+
+            pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX6 xwax:right_timecode_L
+            pw-link alsa_input.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-input:capture_AUX7 xwax:right_timecode_R
+
+            pw-link xwax:left_playback_L alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX2
+            pw-link xwax:left_playback_R alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX3
+
+            pw-link xwax:right_playback_L alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX4
+            pw-link xwax:right_playback_R alsa_output.usb-Allen_and_Heath_XONE_96_USB_2-00.multichannel-output:playback_AUX5
+
+            fg
+          '';
+        })
+        abcde
+        alsa-scarlett-gui
+        audacity
+        bitwig-studio5
+        elektroid
+        mixxx
+        pipecontrol
+        qpwgraph
+        xwax-beta
+      ];
   };
 }
