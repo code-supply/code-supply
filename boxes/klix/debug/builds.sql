b90278c5a6d793baf1c6e8fea89bcb2f4030d4e5
Andrew Bruce <me@andrewbruce.net>
2026-01-19 15:14:42 +0000

    whitespace

--- boxes/klix/debug/builds.sql
+++ boxes/klix/debug/builds.sql
@@ -1,4 +1,5 @@
 select u.email, b.id, b.error, i.klipper_config 
 from builds b 
 join images i on i.id = b.image_id 
-join users u on u.id = i.user_id where error is not null
+join users u on u.id = i.user_id
+where error is not null
