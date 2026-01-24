08cb4057a67a476d6e03ce1651a3dd557e355fd5
Andrew Bruce <me@andrewbruce.net>
2026-01-24 12:30:06 +0000

    Klix debugging scripts for testing nixos changes

---
+++ boxes/klix/debug/flake_lock.sql
@@ -0,0 +1,5 @@
+select b.flake_lock
+from builds b 
+join images i on i.id = b.image_id 
+where i.id = :'image_id'
+order by b.id desc;
---
+++ boxes/klix/debug/flake_nix.sql
@@ -0,0 +1,5 @@
+select b.flake_nix
+from builds b 
+join images i on i.id = b.image_id 
+where i.id = :'image_id'
+order by b.id desc;
